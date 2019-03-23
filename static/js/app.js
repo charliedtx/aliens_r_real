815813bcf2ea17389aed97b5cbd298bae32e7c4e
charliedtx <corbintx@gmail.com>
2019-03-22 22:46:23 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -7,15 +7,33 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-data.forEach(function(ufo) {
+data.forEach(function (ufo) {
 
   tbody.append("tr");
-  Object.entries(ufo).forEach(function([key, value]) {
+  Object.entries(ufo).forEach(function ([key, value]) {
 
     console.log(key, value);
 
     var cell = tbody.append("td");
     cell.text(value);
   });
+});
 
+/*
+var submit = d3.select("#submit");
+
+submit.on("click", function() {
+  d3.select("summary").html("");
+
+  var dTime = d3.select("#datetime");
+  var dtimeValue = dTime.property("value");
+  var filteredData = tableData.filter(tableData => tableData.datetime === dtimeValue);
+
+  filteredData.forEach((ufoDate) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoDate).forEach([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    }
+  });
 });
